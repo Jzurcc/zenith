02b5476d1dc8413e919be40b4be300c05ba7842d
Ranna <balaybayrjanna@gmail.com>
2025-11-16 21:32:43 +0800

    rjanna revision #1

--- app/src/main/java/com/cc17/zenith/PatientInfo.java
+++ app/src/main/java/com/cc17/zenith/PatientInfo.java
@@ -1,19 +1,27 @@
 package com.cc17.zenith;
 
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class PatientInfo extends Fragment {
+
+    // Helper method to find EditText and set text safely
+    // This method signature is designed to work with your provided code snippet.
+    private void setEditText(View view, int id, String text) {
+        EditText et = view.findViewById(id);
+        if (et != null && text != null) {
+            et.setText(text);
+        }
+    }
 
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -25,12 +33,75 @@ public class PatientInfo extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_patient_info, container, false);
+
+        // --- 1. Set Click Listeners for Interactive Elements ---
+
+        // The 'sex' button listener you originally provided
         ImageButton sex = view.findViewById(R.id.sex);
         sex.setOnClickListener(v -> {
-            Toast.makeText(getActivity(), "Secret Toast: yes", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getActivity(), "Sex selection toggled", Toast.LENGTH_SHORT).show();
         });
+
+        // Other ImageButtons
+        view.findViewById(R.id.button2).setOnClickListener(v -> Toast.makeText(getActivity(), "Organ Donor status toggled", Toast.LENGTH_SHORT).show());
+        view.findViewById(R.id.button3).setOnClickListener(v -> Toast.makeText(getActivity(), "Living Will status toggled", Toast.LENGTH_SHORT).show());
+        view.findViewById(R.id.button4).setOnClickListener(v -> Toast.makeText(getActivity(), "Personal Email status toggled", Toast.LENGTH_SHORT).show());
+        view.findViewById(R.id.button5).setOnClickListener(v -> Toast.makeText(getActivity(), "Same Mail Address status toggled", Toast.LENGTH_SHORT).show());
+
+        // QR Code ImageView is also clickable
+        view.findViewById(R.id.imageView32).setOnClickListener(v -> Toast.makeText(getActivity(), "Generating QR code...", Toast.LENGTH_SHORT).show());
+
+        // --- 2. Retrieve and Populate Data ---
+        Bundle args = getArguments();
+        if (args != null) {
+
+            // Basic Info Fields
+            setEditText(view, R.id.et_first_name, args.getString("firstName"));
+            setEditText(view, R.id.et_middle_name, args.getString("middleInitial"));
+            setEditText(view, R.id.et_last_name, args.getString("lastName"));
+            // Preferred name field is typically left empty unless data exists for it
+
+            // Detailed Info Fields (Matching the image snippet and preceding logic)
+            setEditText(view, R.id.et_dob, args.getString("dob"));
+            setEditText(view, R.id.et_country_birth, args.getString("countryOfBirth"));
+            setEditText(view, R.id.et_city_birth, args.getString("cityOfBirth"));
+            setEditText(view, R.id.et_province_birth, args.getString("provinceOfBirth"));
+
+            setEditText(view, R.id.et_marital_status, args.getString("maritalStatus"));
+            setEditText(view, R.id.et_race_ethnicity, args.getString("raceEthnicity"));
+            setEditText(view, R.id.et_mrn, args.getString("mrn"));
+            setEditText(view, R.id.et_fin, args.getString("mrn")); // Assuming FIN uses MRN value for sample data
+
+            setEditText(view, R.id.et_occupation, args.getString("occupation"));
+            setEditText(view, R.id.et_employer, args.getString("employer"));
+            setEditText(view, R.id.et_education, args.getString("education"));
+
+            setEditText(view, R.id.et_religion, args.getString("religion"));
+            setEditText(view, R.id.et_preferences, args.getString("preferences"));
+            setEditText(view, R.id.et_lang_record, args.getString("langRecord"));
+            setEditText(view, R.id.et_lang_record_no, args.getString("langRecordNo"));
+
+            // Contact/Address Fields
+            setEditText(view, R.id.et_email, args.getString("email"));
+
+            setEditText(view, R.id.et_address_line1, args.getString("address1"));
+            setEditText(view, R.id.et_city_address, args.getString("city"));
+            setEditText(view, R.id.et_province_address, args.getString("province"));
+
+            setEditText(view, R.id.et_address_line2, null); // Placeholder for Address Line 2
+            setEditText(view, R.id.et_zipcode, args.getString("zip"));
+            setEditText(view, R.id.et_region, args.getString("region"));
+            setEditText(view, R.id.et_country, args.getString("country"));
+
+            setEditText(view, R.id.et_primary_phone, args.getString("primaryPhone"));
+            setEditText(view, R.id.et_secondary_phone, args.getString("secondaryPhone"));
+
+            setEditText(view, R.id.et_remarks, null); // Placeholder for Remarks
+
+            // Note: Boolean fields (isOrganDonor, isLivingWill, etc.) would require specific logic to toggle the ImageButton background drawable.
+        }
+
         return view;
     }
 }
--- app/src/main/java/com/cc17/zenith/patients.java
+++ app/src/main/java/com/cc17/zenith/patients.java
@@ -1,29 +1,20 @@
 package com.cc17.zenith;
 
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout; // Used for patient list items
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link patients#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class patients extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    // Mimicking the private static final fields for argument keys
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
     private String mParam2;
 
     public patients() {
@@ -38,7 +29,6 @@ public class patients extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment profile.
      */
-    // TODO: Rename and change types and number of parameters
     public static patients newInstance(String param1, String param2) {
         patients fragment = new patients();
         Bundle args = new Bundle();
@@ -50,19 +40,9 @@ public class patients extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-//        ImageButton showMore = findViewById(R.id.imageButton2);
-//        showMore.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                getSupportFragmentManager()
-//                        .beginTransaction()
-//                        .replace(R.id.fragment_layout, diseasetrends())
-//                        .commit();
-//            }
-//        });
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
+            String mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
@@ -71,7 +51,123 @@ public class patients extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_patients, container, false);
+        View view = inflater.inflate(R.layout.fragment_patients, container, false);
+
+        // Find the clickable patient rows defined in XML (assuming LinearLayouts were used)
+        LinearLayout alvarezRow = view.findViewById(R.id.patient_alvarez);
+        LinearLayout bautistaRow = view.findViewById(R.id.patient_bautista);
+        LinearLayout cruzRow = view.findViewById(R.id.patient_cruz);
+        LinearLayout delaRosaRow = view.findViewById(R.id.patient_delarosa);
+
+
+        // Setup click listeners for navigation and data passing
+        alvarezRow.setOnClickListener(v -> navigateToPatientInfo(
+                "Julian", "Ramos", "Alvarez", "46", "Male", "6202158", "0965 0568 555",
+                "12/21/1979", "Philippines", "Baguio City", "Benguet",
+                "Married", "Ilocano", "200365448", "Architect",
+                "Visionarch", "College Graduate", "Christian", "N/A",
+                "English", "1977232", true, true,
+                "julianalvarez@gmail.com", "142 Holy Ghost Hill Ext. Rd.", "Baguio",
+                "Benguet", "2600", "CAR", "Philippines",
+                "(63+) 927 910 7392", "(214) 723-9001"
+        ));
+
+        bautistaRow.setOnClickListener(v -> navigateToPatientInfo(
+                "Angela", "M", "Bautista", "41", "Female", "6203174", "0917 2256 432",
+                "10/15/1984", "Philippines", "Manila", "Metro Manila",
+                "Single", "Tagalog", "200365449", "Designer",
+                "Self-Employed", "High School Graduate", "Catholic", "None",
+                "Tagalog", "1000001", false, false,
+                "angelabautista@gmail.com", "123 Main St.", "Manila",
+                "Metro Manila", "1000", "NCR", "Philippines",
+                "(63+) 917 2256 432", "(02) 8123-4567"
+        ));
+
+        cruzRow.setOnClickListener(v -> navigateToPatientInfo(
+                "Michael", "J", "Cruz", "42", "Male", "6204180", "0995 8457 210",
+                "05/01/1983", "Philippines", "Cebu City", "Cebu",
+                "Married", "Cebuano", "200365450", "Engineer",
+                "MegaWorld", "Masters Degree", "Catholic", "N/A",
+                "Cebuano", "2000002", true, false,
+                "michaelcruz@gmail.com", "456 Ocean View Rd.", "Cebu",
+                "Cebu", "6000", "VII", "Philippines",
+                "(63+) 995 8457 210", "(032) 567-8901"
+        ));
+
+        delaRosaRow.setOnClickListener(v -> navigateToPatientInfo(
+                "Camille", "A", "Dela Rosa", "27", "Female", "6202198", "0921 7789 654",
+                "11/20/1998", "Philippines", "Davao City", "Davao Del Sur",
+                "Single", "Bicolano", "200365451", "Student",
+                "None", "Undergraduate", "Atheist", "Vegan",
+                "English", "3000003", false, false,
+                "camille.dela.rosa@gmail.com", "789 Pine Tree Lane", "Davao",
+                "Davao Del Sur", "8000", "XI", "Philippines",
+                "(63+) 921 7789 654", "N/A"
+        ));
+
+        return view;
+    }
+
+    /**
+     * Helper function to bundle and navigate with all patient data.
+     */
+    private void navigateToPatientInfo(
+            String firstName, String middleInitial, String lastName, String age, String sex, String idNo, String mobileNo,
+            String dob, String countryOfBirth, String cityOfBirth, String provinceOfBirth,
+            String maritalStatus, String raceEthnicity, String mrn, String occupation,
+            String employer, String education, String religion, String preferences,
+            String langRecord, String langRecordNo, boolean isOrganDonor, boolean isLivingWill,
+            String email, String address1, String city, String province, String zip, String region, String country,
+            String primaryPhone, String secondaryPhone
+    ) {
+        Bundle bundle = new Bundle();
+
+        // Basic Info
+        bundle.putString("firstName", firstName);
+        bundle.putString("middleInitial", middleInitial);
+        bundle.putString("lastName", lastName);
+        bundle.putString("age", age);
+        bundle.putString("sex", sex);
+        bundle.putString("idNo", idNo);
+        bundle.putString("mobileNo", mobileNo);
+
+        // Detailed Info
+        bundle.putString("dob", dob);
+        bundle.putString("countryOfBirth", countryOfBirth);
+        bundle.putString("cityOfBirth", cityOfBirth);
+        bundle.putString("provinceOfBirth", provinceOfBirth);
+        bundle.putString("maritalStatus", maritalStatus);
+        bundle.putString("raceEthnicity", raceEthnicity);
+        bundle.putString("mrn", mrn);
+        bundle.putString("occupation", occupation);
+        bundle.putString("employer", employer);
+        bundle.putString("education", education);
+        bundle.putString("religion", religion);
+        bundle.putString("preferences", preferences);
+        bundle.putString("langRecord", langRecord);
+        bundle.putString("langRecordNo", langRecordNo);
+        bundle.putBoolean("isOrganDonor", isOrganDonor);
+        bundle.putBoolean("isLivingWill", isLivingWill);
+
+        // Contact/Address
+        bundle.putString("email", email);
+        bundle.putString("address1", address1);
+        bundle.putString("city", city);
+        bundle.putString("province", province);
+        bundle.putString("zip", zip);
+        bundle.putString("region", region);
+        bundle.putString("country", country);
+        bundle.putString("primaryPhone", primaryPhone);
+        bundle.putString("secondaryPhone", secondaryPhone);
+
+        var patientInfoFragment = new PatientInfo();
+        patientInfoFragment.setArguments(bundle);
+
+        // Navigate to the PatientInfo fragment (replace R.id.fragment_layout with your actual container ID)
+        int commit = requireActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_layout, patientInfoFragment)
+                .addToBackStack(null)
+                .commit();
     }
 
     @Override
