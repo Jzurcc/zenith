45172c318880180660e1092435a80bea9d21f26f
jeniann <jtg6745@students.uc-bcf.edu.ph>
2025-12-02 14:18:28 +0800

    add navigation flags to QRScanner to preserve ViewModel state

--- app/src/main/java/com/cc17/zenith/QRScanner.java
+++ app/src/main/java/com/cc17/zenith/QRScanner.java
@@ -86,6 +86,7 @@ public class QRScanner extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(QRScanner.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP); // so the data will still be intact
                 startActivity(intent);
             }
         });
